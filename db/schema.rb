1b2e88e8289c07e5c1c41263e3ce0893a96bee32
ShogoAkiyama <15t4001r@vc.ibaraki.ac.jp>
2017-11-22 21:25:26 +0900

    schemaをコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122042946) do
+ActiveRecord::Schema.define(version: 20171122111805) do
 
-  create_table "academic_canledars", force: :cascade do |t|
+  create_table "academic_calendars", force: :cascade do |t|
     t.date "no_lecture_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
