b119eacb31df0a938258188846762c5cb5ea7d70
ShogoAkiyama <15t4001r@vc.ibaraki.ac.jp>
2017-11-25 22:39:20 +0900

    管理者機能を作成

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ApplicationRecord
-    has_many :student_lectures
+    has_many :student_lectures, dependent: :destroy
     has_many :lectures, through: :student_lectures
 end
--- app/models/teacher_lecture.rb
+++ app/models/teacher_lecture.rb
@@ -2,7 +2,7 @@ class TeacherLecture < ApplicationRecord
   belongs_to :teacher
   belongs_to :lecture
   
-  has_many :cancel_lectures
-  has_many :supplement_lectures
-  has_many :reports
+  has_many :cancel_lectures, dependent: :destroy
+  has_many :supplement_lectures, dependent: :destroy
+  has_many :reports, dependent: :destroy
 end
