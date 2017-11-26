61350b37404b2e9d982a4a256124ee62a8be71af
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2017-11-26 09:53:20 +0900

    不要なファイル、コード削除

--- config/routes.rb
+++ config/routes.rb
@@ -30,12 +30,6 @@ Rails.application.routes.draw do
   root 'welcom#home'
 
 
-  
-  #get 'student_base/rogin'
-  #get 'teacher_base/rogin'
-  #get 'staff_base/rogin'
-  #get 'admin/rogin'
-
   get 'authorize' => 'auth#gettoken'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
