7b0492e4609d797bbf43d8035058909233b5c5a4
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2017-11-26 10:59:44 +0900

    事務員　講義一括登録　作成

--- app/controllers/staff_operate_lectures_controller.rb
+++ app/controllers/staff_operate_lectures_controller.rb
@@ -9,6 +9,8 @@ class StaffOperateLecturesController < StaffBaseController
 
   #講義登録
   def create
+    Lecture.import params[:file] #csv import実装
+    redirect_to new_staff_operate_lectures_path
   end
 
   #講義検索
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -8,4 +8,18 @@ class Lecture < ApplicationRecord
     has_many :cancel_lectures, through: :teacher_lectures
     has_many :supplement_lectures, through: :teacher_lectures
     has_many :reports, through: :teacher_lectures
+
+    def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+
+        obj = new
+        obj.attributes = row.to_hash.slice(*updatable_attributes)
+
+        obj.save!
+      end
+    end
+
+    def self.updatable_attributes
+      ["syllabus_code","name","semester","year","is_intensive","day","period","faculty","department"] #登録可能なデータをバリデーション
+    end
 end
