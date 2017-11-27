375724dd23175769a238f2b8d59869ae32e3899e
ShogoAkiyama <15t4001r@vc.ibaraki.ac.jp>
2017-11-27 21:01:03 +0900

    adminをdeviseで作成

--- app/controllers/admin_base_controller.rb
+++ app/controllers/admin_base_controller.rb
@@ -1,4 +1,8 @@
 class AdminBaseController < ApplicationController
+  def after_sign_in_path_for(resource)
+    admin_homes_path
+  end
+  
   def rogin
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
   resources :admin_homes, only: [:index]
   #resources
   #admin
