45b0f4cd96f7fa11f1e3c85a4ed73c20c059b529
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2017-11-30 10:56:01 +0900

    事務員用レイアウト作成

--- app/controllers/staff_operate_lectures_controller.rb
+++ app/controllers/staff_operate_lectures_controller.rb
@@ -1,7 +1,7 @@
 class StaffOperateLecturesController < StaffBaseController
   #講義一覧表示画面
   def index
-      @lectures = Lecture.all
+      @lectures = Lecture.page(params[:page])
   end
 
   #講義追加画面
@@ -16,7 +16,7 @@ class StaffOperateLecturesController < StaffBaseController
 
   #講義検索
   def search
-    @lectures = Lecture.search(params)
+    @lectures = Lecture.search(params).page(params[:page])
     render 'index'
   end
 
