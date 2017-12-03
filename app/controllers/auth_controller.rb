5945ab6ac806505c837488b4ac3d0a5bccc09133
Nenpi100 <yk.chi-chan@i.softbank.jp>
2017-12-03 23:43:05 +0900

    '未ログインの時セッション破壊処理から省く'

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -26,7 +26,7 @@ include AuthHelper
                     #新規作成
                     Student.create(email: @email, azure_token: token.to_hash)
                 end
-                session[:student_login_flag] true
+                session[:student_login_flag] = true
                 redirect_to  controller: 'student_homes', action: 'index'
             else
                 redirect_to controller: 'welcom', action: 'home'
@@ -36,14 +36,20 @@ include AuthHelper
 
     end
 
-    # 燃費くん改修よろしく！終わったらこのメッセージ消してね！
-    #　:hogehogeに適切な変数突っ込むとセッションとcokkieに入ってるデータが破壊されるはず！(:hogehogeは自分で埋めていただきたく！)
+    
     def logout
+        if session[:stff_login_flag]==nil || session[:teacher_login_flag]==nil || session[:student_login_flag] == nil
+            redirect_to root_path,:notice => 'ログインしていません。'
+        
+        else
         session[:staff_login_flag] = nil unless session[:staff_login_flag].nil?
         session[:teacher_login_flag] = nil unless session[:teacher_login_flag].nil?
         session[:student_login_flag] = nil unless session[:student_login_flag].nil?
-        cookies.delete(:hogehoge)
+        cookies.delete(:value)
         redirect_to root_path,:notice => '正常にログアウトできました。'
+
+        end
+        
     end
 
 end
