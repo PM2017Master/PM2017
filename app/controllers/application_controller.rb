e47cc1219864da9c043083ca9d4dcc33eba5dd2b
Nenpi100 <yk.chi-chan@i.softbank.jp>
2017-12-10 01:16:21 +0900

    例えば、事務員がログアウトせずにスタッフのURLを入力するとスタッフページに飛んでしまうエラーを解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,26 @@ class ApplicationController < ActionController::Base
   before_action :check_login?, if: :needs_check?
 
   def check_login?
-    if session[:user_email].nil?
+    xemail = session[:user_email]
+    if xemail.nil?
+      redirect_to root_path  
+    elsif controller_name.include?('teacher') && !(Teacher.exists?(email: xemail))
       redirect_to root_path
+    elsif controller_name.include?('staff') && !(Staff.exists?(email: session[:user_email]))
+      redirect_to root_path
+    else
+      check = xemail.include?("student")
+      if controller_name.include?('student') && !(check)
+         redirect_to root_path
+      end
     end
   end
 
+
   def needs_check?
     logger.debug controller_name
-    unless controller_name.include?('staff') || controller_name.include?('teacher') || controller_name.include?('student')
-      false
+    if controller_name.include?('welcom') || controller_name.include?('auth') || controller_name.include?('admin')
+    false
     else
       true
     end
