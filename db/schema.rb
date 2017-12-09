e4783492ae136da40f6381e5f4898b40f85eaf9a
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2017-12-09 12:52:25 +0900

    seed変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130035717) do
+ActiveRecord::Schema.define(version: 20171130101216) do
 
   create_table "academic_calendars", force: :cascade do |t|
     t.date "no_lecture_date", null: false
@@ -37,12 +37,12 @@ ActiveRecord::Schema.define(version: 20171130035717) do
   end
 
   create_table "cancel_lectures", force: :cascade do |t|
-    t.integer "TeacherLecture_id"
+    t.integer "teacher_lecture_id"
     t.date "cancel_date", null: false
     t.string "period", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["TeacherLecture_id"], name: "index_cancel_lectures_on_TeacherLecture_id"
+    t.index ["teacher_lecture_id"], name: "index_cancel_lectures_on_teacher_lecture_id"
   end
 
   create_table "lectures", force: :cascade do |t|
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20171130035717) do
     t.boolean "is_intensive", default: false
     t.string "day"
     t.integer "period"
+    t.string "faculty", null: false
+    t.string "department"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "teacher_name"
@@ -66,13 +68,13 @@ ActiveRecord::Schema.define(version: 20171130035717) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.integer "TeacherLecture_id"
+    t.integer "teacher_lecture_id"
     t.string "title"
     t.text "content"
     t.date "deadline_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["TeacherLecture_id"], name: "index_reports_on_TeacherLecture_id"
+    t.index ["teacher_lecture_id"], name: "index_reports_on_teacher_lecture_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -88,15 +90,16 @@ ActiveRecord::Schema.define(version: 20171130035717) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_staffs_on_email", unique: true
   end
 
   create_table "student_lectures", force: :cascade do |t|
-    t.integer "Student_id"
-    t.integer "Lecture_id"
+    t.integer "student_id"
+    t.integer "lecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["Lecture_id"], name: "index_student_lectures_on_Lecture_id"
-    t.index ["Student_id"], name: "index_student_lectures_on_Student_id"
+    t.index ["lecture_id"], name: "index_student_lectures_on_lecture_id"
+    t.index ["student_id"], name: "index_student_lectures_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -108,21 +111,22 @@ ActiveRecord::Schema.define(version: 20171130035717) do
   end
 
   create_table "supplement_lectures", force: :cascade do |t|
-    t.integer "TeacherLecture_id"
+    t.integer "teacher_lecture_id"
     t.date "supplement_date", null: false
     t.string "period", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["TeacherLecture_id"], name: "index_supplement_lectures_on_TeacherLecture_id"
+    t.index ["teacher_lecture_id"], name: "index_supplement_lectures_on_teacher_lecture_id"
   end
 
   create_table "teacher_lectures", force: :cascade do |t|
-    t.integer "Teacher_id"
-    t.integer "Lecture_id"
+    t.integer "teacher_id"
+    t.integer "lecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["Lecture_id"], name: "index_teacher_lectures_on_Lecture_id"
-    t.index ["Teacher_id"], name: "index_teacher_lectures_on_Teacher_id"
+    t.index ["lecture_id"], name: "index_teacher_lectures_on_lecture_id"
+    t.index ["teacher_id", "lecture_id"], name: "index_teacher_lectures_on_teacher_id_and_lecture_id", unique: true
+    t.index ["teacher_id"], name: "index_teacher_lectures_on_teacher_id"
   end
 
   create_table "teachers", force: :cascade do |t|
@@ -130,6 +134,7 @@ ActiveRecord::Schema.define(version: 20171130035717) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_teachers_on_email", unique: true
   end
 
 end
--- db/seeds/people.seeds.rb
+++ db/seeds/people.seeds.rb
@@ -5,11 +5,14 @@ Student.create(email: "pmstudent2@outlook.com")
 Student.create(email: "pmstudent3@outlook.com")
 
 #教員
-t1 = Teacher.new(email: "pmteacher2017@outlook.com", name:"上田賀一")
-t2 = Teacher.new(email: "pmteacher@outlook.com", name:"岡田信一郎")
-
-t1.save
-t2.save
+Teacher.create(email: "pmteacher2017@outlook.com", name:"上田賀一")
+Teacher.create(email: "pmteacher@outlook.com", name:"岡田信一郎")
+Teacher.create(email: "pmteacher1@outlook.com", name:"秋山先生")
+Teacher.create(email: "pmteacher2@outlook.com", name:"工学部教員")
+Teacher.create(email: "pmteacher3@outlook.com", name:"人文学部教員")
+Teacher.create(email: "pmteacher4@outlook.com", name:"理学部教員")
+Teacher.create(email: "pmteacher5@outlook.com", name:"農学部教員")
+Teacher.create(email: "pmteacher6@outlook.com", name:"教育学部教員")
 
 #事務員
 Staff.create(email: "pmstaff2017@outlook.jp")
