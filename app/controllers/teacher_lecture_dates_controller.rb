a22b9b94d4843cec3d5947580f4af3b7334df109
tanakadaisuke0226 <15t4045a@vc.ibaraki.ac.jp>
2017-12-10 20:19:02 +0900

    create all function

--- app/controllers/teacher_lecture_dates_controller.rb
+++ app/controllers/teacher_lecture_dates_controller.rb
@@ -2,10 +2,56 @@ class TeacherLectureDatesController < TeacherBaseController
   #開講日変更登録画面
   def new
     @lectures = Teacher.find_by(:email => session[:user_email]).lectures
-    @date =
+    @date = CancelLecture.new
   end
 
   #DBに追加
   def create
+    if date_params[:cancel_date_previous_change] == "休講"
+      if SupplementLecture.where(teacher_lecture_id: get_teacher_lecture_id).where(supplement_date: date_params[:cancel_date]).where(period: date_params[:period]).count <=0
+        @cancel_lectures = CancelLecture.create(teacher_lecture_id: get_teacher_lecture_id, cancel_date: date_params[:cancel_date], period: date_params[:period])
+        if @cancel_lectures.valid?
+          @cancel_lectures.save!
+          redirect_to teacher_homes_path, :notice => '休講の登録が完了しました。'
+        else
+          redirect_to new_teacher_lecture_date_path, :alert =>'休講の登録に失敗しました。入力値を確認してください！'
+        end
+      else
+        #SupplementLecture.destroy_all.(teacher_lecture_id: get_teacher_lecture_id, supplement_date: date_params[:cancel_date], period: date_params[:period])
+        SupplementLecture.where(teacher_lecture_id: get_teacher_lecture_id).where(supplement_date: date_params[:cancel_date]).where(period: date_params[:period]).destroy_all
+        redirect_to teacher_homes_path, :notice => '補講の削除が完了しました。'
+      end
+    else
+      if CancelLecture.where(teacher_lecture_id: get_teacher_lecture_id).where(cancel_date: date_params[:cancel_date]).where(period: date_params[:period]).count <=0
+        @supplement_lectures = SupplementLecture.create(teacher_lecture_id: get_teacher_lecture_id, supplement_date: date_params[:cancel_date], period: date_params[:period])
+        if @supplement_lectures.valid?
+          @supplement_lectures.save!
+          redirect_to teacher_homes_path, :notice => '補講の登録が完了しました。'
+        else
+          redirect_to new_teacher_lecture_date_path, :alert =>'補講の登録に失敗しました。入力値を確認してください！'
+        end
+      else
+        #CancelLecture.destroy_all.(teacher_lecture_id: get_teacher_lecture_id, cancel_date: date_params[:cancel_date], period: date_params[:period])
+        CancelLecture.where(teacher_lecture_id: get_teacher_lecture_id).where(cancel_date: date_params[:cancel_date]).where(period: date_params[:period]).destroy_all
+        redirect_to teacher_homes_path, :notice => '休講の削除が完了しました。'
+      end
+    end
   end
+
+  private
+  def date_params
+    params
+      .require(:cancel_lecture)
+      .permit(:lecture, :cancel_date_previous_change, :cancel_date, :period)
+  end
+
+  def get_teacher_lecture_id
+    get_teacher = Teacher.find_by(:email => session[:user_email])
+    get_lecture = get_teacher.lectures.find_by(:name => date_params[:lecture])
+    if get_teacher.present? && get_lecture.present?
+      need_data = TeacherLecture.find_by(:Teacher_id => get_teacher.id, :Lecture_id => get_lecture.id)
+      need_data.id
+    end
+  end
+
 end
--- app/models/supplement_lecture.rb
+++ app/models/supplement_lecture.rb
@@ -3,4 +3,8 @@ class SupplementLecture < ApplicationRecord
 
   has_one :teacher, through: :teacher_lecture
   has_one :lecture, through: :teacher_lecture
+
+  validates :teacher_lecture_id, presence: true
+  validates :supplement_date, presence: true
+  validates :period, presence: true
 end
