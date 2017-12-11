938c4c646e42dd7d6d191179fa144135ec767af6
YutoSuzuki <15t4037x@vc.ibaraki.ac.jp>
2017-12-11 10:56:46 +0900

    staffバリデーションエラー画面 修正

--- app/controllers/staff_base_controller.rb
+++ app/controllers/staff_base_controller.rb
@@ -2,7 +2,7 @@
 # その他の事務コントローラはこのコントローラを継承すること。
 class StaffBaseController < ApplicationController
   layout 'staff_base'
-  #rescue_from Exception, with: :render_500
+  rescue_from Exception, with: :render_500
 
   def render_500
   render template: 'errors/error_500', status: 500, layout: 'staff_base', content_type: 'text/html'
