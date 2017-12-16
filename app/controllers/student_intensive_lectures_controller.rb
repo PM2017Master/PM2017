ecd45cd60ff5dbf121e7af024dc33416b30df6fb
15t4041f <15t4041f@vc.ibaraki.ac.jp>
2017-12-16 17:14:58 +0900

    Fix intensive_lecture search function

--- app/controllers/student_intensive_lectures_controller.rb
+++ app/controllers/student_intensive_lectures_controller.rb
@@ -1,15 +1,13 @@
 ﻿class StudentIntensiveLecturesController < StudentBaseController
   #集中講義一覧表示画面
   def index
-  	if params[:faculty] || params[:department] || params[:search_word]
-  		@intensive_lectures = Lecture.where(is_intensive: true)
-  		.where('faculty LIKE ?', "%#{params[:faculty]}%")
-  		.where('department LIKE ?', "%#{params[:department]}%")
-  		.where('name LIKE ?', "%#{params[:search_word]}%")
-  	else
-	  	@intensive_lectures = Lecture.where(is_intensive: true)
-  	end
-  	
+		@intensive_lectures = []
+		lectures = Lecture.search(params)
+		lectures.each do |lecture|
+			if lecture.is_intensive == true
+				@intensive_lectures.push(lecture)
+			end
+		end
   end
   
   #集中講義登録画面
